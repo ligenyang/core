1d14ca51c3f490323edfe7aeb509352968e72b9e
ligenyang <41351858+ligenyang@users.noreply.github.com>
2018-11-29 16:03:08 +0800

    init

--- src/main/java/com/w/core/CoreApplication.java
+++ src/main/java/com/w/core/CoreApplication.java
@@ -2,8 +2,9 @@ package com.w.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class CoreApplication {
 
 	public static void main(String[] args) {
