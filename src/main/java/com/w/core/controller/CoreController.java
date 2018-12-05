ee1388001ddb91e02fa0913f7136c82a2c48464b
ligenyang <41351858+ligenyang@users.noreply.github.com>
2018-12-05 18:01:45 +0800

    init

--- src/main/java/com/w/core/controller/CoreController.java
+++ src/main/java/com/w/core/controller/CoreController.java
@@ -1,4 +1,15 @@
 package com.w.core.controller;
 
-public class CoreController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CoreController<E, S> {
+
+
+    @RequestMapping("{core}")
+    public String core(@PathVariable String core) {
+        return core;
+    }
 }
--- src/main/java/com/w/core/controller/UserController.java
+++ src/main/java/com/w/core/controller/UserController.java
@@ -2,7 +2,6 @@ package com.w.core.controller;
 
 import com.w.core.model.User;
 import com.w.core.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +10,13 @@ import javax.annotation.Resource;
 import java.util.List;
 
 @Controller
-@RequestMapping(value = "/user")
+@RequestMapping(value = "user")
 public class UserController {
 
     @Resource
     private UserService userService;
 
-    @RequestMapping(value = "/select")
+    @RequestMapping(value = "select")
     public String select(Model model) {
         List<User> userModels = userService.select();
         return "index";
